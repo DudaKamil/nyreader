b439c8a2feeb9b2b2201ac4e7f5e96c5ac9734f2
Kamil Duda <kamilduda01@gmail.com>
2016-04-23 20:30:10 +0200

    Add localization resources

--- src/main/webapp/app/components/application.component.ts
+++ src/main/webapp/app/components/application.component.ts
@@ -1,16 +1,28 @@
 import {Component} from "angular2/core";
 import {RouteConfig, ROUTER_DIRECTIVES} from "angular2/router";
 import {WelcomeComponent} from "./welcome/welcome.component";
+import {TranslatePipe, LocaleService, LocalizationService} from "angular2localization/angular2localization";
 
 @Component({
     selector: "application",
     templateUrl: "app/components/application.component.html",
     styleUrls: ["app/components/application.component.css"],
-    directives: [ROUTER_DIRECTIVES]
+    directives: [ROUTER_DIRECTIVES],
+    providers: [LocaleService, LocalizationService],
+    pipes: [TranslatePipe]
 })
 @RouteConfig([
     {path: "/welcome", name: "Welcome", component: WelcomeComponent},
     {path: "/**", redirectTo: ["Welcome"]}
 ])
 export class ApplicationComponent {
+    constructor(public locale: LocaleService, public localization: LocalizationService) {
+        // TODO: check lang resources
+        this.localization.translationProvider("../locale/locale-");
+        this.locale.setCurrentLanguage("pl");
+    }
+
+    changeLanguage(lang: string) {
+        this.locale.setCurrentLanguage(lang);
+    }
 }
