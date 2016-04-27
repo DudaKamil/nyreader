64ef744209d70229d26b5f74ebce131d2a05a05a
Kamil Duda <kamilduda01@gmail.com>
2016-04-27 16:19:45 +0200

    Set up ng2-translate

--- src/main/webapp/app/components/application.component.ts
+++ src/main/webapp/app/components/application.component.ts
@@ -1,14 +1,13 @@
 import {Component} from "angular2/core";
 import {RouteConfig, ROUTER_DIRECTIVES} from "angular2/router";
 import {WelcomeComponent} from "./welcome/welcome.component";
-import {TranslatePipe, LocaleService, LocalizationService} from "angular2localization/angular2localization";
+import {TranslatePipe, TranslateService} from "ng2-translate";
 
 @Component({
     selector: "application",
     templateUrl: "app/components/application.component.html",
     styleUrls: ["app/components/application.component.css"],
     directives: [ROUTER_DIRECTIVES],
-    providers: [LocaleService, LocalizationService],
     pipes: [TranslatePipe]
 })
 @RouteConfig([
@@ -16,13 +15,20 @@ import {TranslatePipe, LocaleService, LocalizationService} from "angular2localiz
     {path: "/**", redirectTo: ["Welcome"]}
 ])
 export class ApplicationComponent {
-    constructor(public locale: LocaleService, public localization: LocalizationService) {
-        // TODO: check lang resources
-        this.localization.translationProvider("../locale/locale-");
-        this.locale.setCurrentLanguage("pl");
+
+    constructor(private translate: TranslateService) {
+        this.translate = translate;
+        let userLang = navigator.language.split("-")[0]; // use navigator lang if available
+        userLang = /(pl|en)/gi.test(userLang) ? userLang : "en";
+
+        // this language will be used as a fallback when a translation isn't found in the current language
+        translate.setDefaultLang("en");
+
+        // the lang to use, if the lang isn't available, it will use the current loader to get them
+        translate.use(userLang);
     }
 
-    changeLanguage(lang: string) {
-        this.locale.setCurrentLanguage(lang);
+    changeLanguate(language: string) {
+        this.translate.use(language);
     }
 }
--- src/main/webapp/app/main.ts
+++ src/main/webapp/app/main.ts
@@ -1,6 +1,11 @@
 import {bootstrap} from "angular2/platform/browser";
 import {ROUTER_PROVIDERS} from "angular2/router";
-import {HTTP_PROVIDERS} from "angular2/http";
+import {HTTP_PROVIDERS, Http} from "angular2/http";
 import {ApplicationComponent} from "./components/application.component";
+import {provide} from "angular2/core";
+import {TRANSLATE_PROVIDERS, TranslateLoader, TranslateStaticLoader} from "ng2-translate";
 
-bootstrap(ApplicationComponent, [ROUTER_PROVIDERS, HTTP_PROVIDERS]);
+bootstrap(ApplicationComponent, [ROUTER_PROVIDERS, HTTP_PROVIDERS, TRANSLATE_PROVIDERS, provide(TranslateLoader, {
+    useFactory: (http: Http) => new TranslateStaticLoader(http, "app/locale", ".json"),
+    deps: [Http]
+})]);
