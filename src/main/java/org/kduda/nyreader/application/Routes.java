fbfd947679b0d87c4a08710f2950c13b3dbf20b9
Kamil Duda <kamilduda01@gmail.com>
2016-04-30 18:10:02 +0200

    Update Security config

--- src/main/java/org/kduda/nyreader/application/Routes.java
+++ src/main/java/org/kduda/nyreader/application/Routes.java
@@ -1,14 +1,14 @@
 package org.kduda.nyreader.application;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.security.Principal;
 
 @RestController
 public class Routes {
-	@RequestMapping("/api/user")
-	public Principal user(Principal user) {
-		return user;
+	@RequestMapping(value = "/authentication/login/process", method = RequestMethod.POST)
+	public String process() {
+		System.out.println("/authentication/login/process");
+		return "zalogowano";
 	}
 }
--- src/main/java/org/kduda/nyreader/configuration/SecurityConfiguration.java
+++ src/main/java/org/kduda/nyreader/configuration/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package org.kduda.nyreader.configuration;
 import org.kduda.nyreader.common.User.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.stereotype.Component;
@@ -16,5 +17,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Autowired
 	public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsServiceImpl);
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests().antMatchers("/**").permitAll()
+			.and().formLogin().loginPage("/login")
+			.loginProcessingUrl("/authentication/login/process");
 	}
 }
