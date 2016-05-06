18a3c1897d258309201eab45506fd076b1db9984
Kamil Duda <kamilduda01@gmail.com>
2016-05-06 20:54:02 +0200

    Add saving token to local storage after login

--- src/main/webapp/app/services/authentication.service.ts
+++ src/main/webapp/app/services/authentication.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from "angular2/core";
-import {Http, Headers, RequestOptions} from "angular2/http";
+import {Http, Headers, RequestOptions, Response} from "angular2/http";
 import {User} from "../common/user";
 import {Observable} from "rxjs";
 
@@ -8,7 +8,7 @@ export class AuthenticationService {
     constructor(private _http: Http) {
     }
 
-    authenticate(user: User): Observable<any> {
+    authenticate(user: User): Observable<Response> {
         let userData: string = JSON.stringify(user);
         let headers = new Headers({
             "Content-Type": "application/json; charset=utf-8",
@@ -16,6 +16,16 @@ export class AuthenticationService {
         });
         let options = new RequestOptions({headers: headers});
 
-        return this._http.post("/auth/login", userData, options);
+        // TODO: map method does not work
+        let response: Observable<Response> = this._http.post("/auth/login", userData, options);
+
+        response.subscribe(
+            res => {
+                let token: string = res.json().token;
+                localStorage.setItem("TOKEN", token);
+            }
+        );
+
+        return response;
     }
 }
